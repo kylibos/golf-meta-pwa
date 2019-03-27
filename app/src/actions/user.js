8ca75dfb1123ef698f71704d91c0276674e3d826
Colin Cannon <colincannon@kylibos.com>
2019-03-26 21:09:59 -0400

    removed console log

--- app/src/actions/user.js
+++ app/src/actions/user.js
@@ -2,6 +2,7 @@ export const SIGN_IN_USER = 'SIGN_IN_USER';
 export const SIGN_OUT_USER = 'SIGN_OUT_USER';
 
 export const signInUser = (user) => {
+	console.log('user', user.uid);
   return {
     type: SIGN_IN_USER,
     user
